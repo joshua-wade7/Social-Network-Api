7b53c80e39215d7f0284464172a17a5f496d5cde
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-04-05 22:08:43 -0600

    added GET, POST, and PUT requests for api/users route in userController file

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,21 @@
 //require in the models needed to transact against
 
+// /api/thoughts
+
+//GET route to get all thoughts
+
+//GET route to get a single thought by its _id
+
+//POST route to create a new thought (need to push created thought's _id to the associated user's thoughts array field)
+
+//PUT route to update a thought by its _id
+
+//DELETE route to remove a thought by its _id
+
+// /api/thoughts/:thoughtId/reactions
+
+//POST route to create a reaction stored in a single thought's reactions array field
+
+//DELETE route to pull and remove a reaction by the reactions reactionId value
+
 //construct all the routes necessary to hit.  Look at mini project for guidance.
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,65 @@
 //require in the models needed to transact against
 
+const User = require("../models/User");
+
+//api/users
+module.exports = {
+  //GET route to get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  //GET route a single user by its _id and populated thought and friend data
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  //POST route to create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  //PUT route to update a user by its _id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this Id" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  //DELETE route to remove a user by its _id
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with this Id" })
+          : User.deleteOne({ _id: req.params.userid })
+      )
+      .then(() => res.json({ message: "User deleted!" }))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // /api/users/:userId/friends/:friendsId
+
+  //POST route to add a new friend to a user's friend list
+
+  //DELETE route to remove a friend from a user's friend list
+};
+
 //setup all the routes necessary to hit.  Look at mini project for guidance
